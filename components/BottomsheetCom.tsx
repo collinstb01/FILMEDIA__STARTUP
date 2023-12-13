b8ce204b36b4175200130ea363b6d65ad5f60359
josephwild <josephomotade0@gmail.com>
2023-12-13 03:00:14 +0100

    commited

--- components/BottomsheetCom.tsx
+++ components/BottomsheetCom.tsx
@@ -1,7 +1,7 @@
-import { View, Text, Image, Pressable } from "react-native";
+import { View, Text, Image, Pressable, Alert } from "react-native";
 import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import Ionicons from "@expo/vector-icons/Ionicons";
+import * as Clipboard from "expo-clipboard";
 import { TouchableOpacity } from "react-native";
 import { router } from "expo-router";
 
@@ -19,11 +19,13 @@ const BottomsheetCom = () => {
       id: 2,
       action: null,
     },
-    {
-      icon: "hand-holding-usd",
-      name: "Borrow",
-    },
   ];
+
+  const copyToClipboard = async (text: string) => {
+    await Clipboard.setStringAsync(text);
+    Alert.alert("Address copied")
+  };
+
   return (
     <View>
       <View className="items-center justify-center">
@@ -33,35 +35,19 @@ const BottomsheetCom = () => {
           }}
           className="w-[90px] h-[90px] rounded-full"
         />
-        <Pressable className="bg-[#001F3F] flex-row space-x-4 items-center rounded-[40px] mt-2 px-[30px] py-[12px]">
+        <Pressable onPress={() => copyToClipboard("0xCD387d3D...DEC6B54D7819D")} className="bg-[#001F3F] flex-row space-x-4 items-center rounded-[40px] mt-2 px-[30px] py-[12px]">
           <Text className="text-[12px] text-[#fff] font-bold">
             0xCD387d3D...DEC6B54D7819D
           </Text>
           <FontAwesome name="copy" size={24} color="#fff" />
         </Pressable>
-        {/** Transaction section */}
-        <View className="min-w-full flex-row items-center justify-evenly mt-[18px]">
-          {transact.map((item) => (
-            <Pressable onPress={item.action} key={item.id}>
-              <View key={item.id} className="items-center">
-                <TouchableOpacity
-                  key={item.id}
-                  className="w-[46px] h-[46px] rounded-full bg-[#3f94e9] flex-row items-center justify-center px-[9px]"
-                >
-                  <FontAwesome
-                    name={item.icon}
-                    size={20}
-                    color="#fff"
-                    className="mt-[8px] ml-[8px]"
-                  />
-                </TouchableOpacity>
-                <Text className="text-[12px] text-[#fff] font-normal">{item.name}</Text>
-              </View>
-            </Pressable>
-          ))}
-        </View>
-        {/** Transaction section end */}
       </View>
+      <View className="min-w-full items-center justify-center mt-[18px]">
+        <Text className="text-[40px] font-bold text-[#fff]">30.00 PEG</Text>
+        <Text className="text-[16px] font-semibold text-gray-400">30$</Text>
+      </View>
+
+      {/** Transaction section */}
       <View className="min-w-full flex-row items-center justify-evenly mt-[18px]">
         {transact.map((item) => (
           <Pressable onPress={item.action} key={item.id}>
@@ -77,11 +63,14 @@ const BottomsheetCom = () => {
                   className="mt-[8px] ml-[8px]"
                 />
               </TouchableOpacity>
-              <Text className="text-[12px] text-[#fff] font-normal">{item.name}</Text>
+              <Text className="text-[12px] text-[#fff] font-normal">
+                {item.name}
+              </Text>
             </View>
           </Pressable>
         ))}
       </View>
+      {/** Transaction section end */}
     </View>
   );
 };
